670aab25dbd173220fa698c0efce05514cb0afd9
Cong-Cong <dacongsama@live.com>
2026-04-20 10:25:51 +0800

    update rspack version

--- rsbuild/ssr-express/src/index.server.tsx
+++ rsbuild/ssr-express/src/index.server.tsx
@@ -3,7 +3,6 @@ import ReactDOMServer from 'react-dom/server';
 import App from './App';
 
 export function render() {
-  console.log('sdfasdfsaf');
   return ReactDOMServer.renderToString(
     <React.StrictMode>
       <App />
