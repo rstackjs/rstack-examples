55e9e9fc495ac7da9f5779cdabf45cc56cf92a7d
Cong-Cong Pan <dacongsama@live.com>
2026-04-20 10:42:30 +0800

    feat: add rspack rsc example (#440)

--- rsbuild/module-federation-enhanced/provider/rsbuild.config.ts
+++ rsbuild/module-federation-enhanced/provider/rsbuild.config.ts
@@ -1,6 +1,6 @@
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 
 export default defineConfig({
   plugins: [
