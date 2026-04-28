a145f5815ad12385376d2c1247a669d66fbbc83b
neverland <chenjiahan.jait@bytedance.com>
2026-04-28 19:10:37 +0800

    fix

--- rslint.config.ts
+++ rslint.config.ts
@@ -2,27 +2,11 @@ import { defineConfig, ts } from '@rslint/core';
 
 export default defineConfig([
   {
-    ignores: [
-      '**/dist/**',
-      '**/dist-*/**',
-      '**/doc_build/**',
-      '**/auto-imports.d.ts',
-      '**/components.d.ts',
-      '**/__snapshots__/**',
-    ],
+    ignores: ['**/dist-*/**', '**/auto-imports.d.ts', '**/components.d.ts', '**/__snapshots__/**'],
   },
+  ts.configs.recommended,
   {
-    ...ts.configs.recommended,
-    files: [
-      'rspack/**/*.{ts,tsx,mts,cts}',
-      'rsbuild/**/*.{ts,tsx,mts,cts}',
-      'rspress/**/*.{ts,tsx,mts,cts}',
-      'rsdoctor/**/*.{ts,tsx,mts,cts}',
-      'rslib/**/*.{ts,tsx,mts,cts}',
-      'rstest/**/*.{ts,tsx,mts,cts}',
-    ],
     rules: {
-      ...ts.configs.recommended.rules,
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
