cc66db2ed0dd228bc105a68113c078c96053e30e
neverland <chenjiahan.jait@bytedance.com>
2026-06-03 22:23:16 +0800

    fix(tanstack-start-rsc): correct include config

--- rsbuild/tanstack-start-rsc/rsbuild.config.ts
+++ rsbuild/tanstack-start-rsc/rsbuild.config.ts
@@ -5,7 +5,9 @@ import { tanstackStart } from '@tanstack/react-start/plugin/rsbuild';
 
 export default defineConfig({
   source: {
-    include: [/[\\/]node_modules[\\/]/],
+    // RSC needs SWC to compile dependencies so directives like "use client" can be detected.
+    // The TanStack Rsbuild plugin will add this automatically when RSC is enabled in the future.
+    include: [{ not: /[\\/]core-js[\\/]/ }],
   },
   plugins: [
     pluginReact(),
