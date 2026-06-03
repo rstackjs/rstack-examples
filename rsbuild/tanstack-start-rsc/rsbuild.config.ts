407bdf7631b34c4c3ca67334aa90a96aefa5ae5a
neverland <chenjiahan.jait@bytedance.com>
2026-06-03 21:48:10 +0800

    feat: improve TanStack Start RSC example

--- rsbuild/tanstack-start-rsc/rsbuild.config.ts
+++ rsbuild/tanstack-start-rsc/rsbuild.config.ts
@@ -4,12 +4,8 @@ import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
 import { tanstackStart } from '@tanstack/react-start/plugin/rsbuild';
 
 export default defineConfig({
-  mode: 'development',
   source: {
     include: [/[\\/]node_modules[\\/]/],
-  },
-  server: {
-    port: 3000,
   },
   plugins: [
     pluginReact(),
