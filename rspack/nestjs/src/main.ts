2a57932c27ee9166acfe353e8317049983c389d9
AsyncIter <intellild@outlook.com>
2026-06-11 20:03:58 +0800

    fix: refine nestjs example (#473)

--- rspack/nestjs/src/main.ts
+++ rspack/nestjs/src/main.ts
@@ -1,14 +1,13 @@
-declare const module: any;
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
+
+  if (import.meta.webpackHot) {
+    import.meta.webpackHot.accept();
+    import.meta.webpackHot.dispose(() => app.close());
   }
 }
 bootstrap();
