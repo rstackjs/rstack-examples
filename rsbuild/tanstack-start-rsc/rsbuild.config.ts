8f9c7449def0c8aa9a82d5784714e80e2c73f0d4
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-22 07:57:44 +0800

    chore(deps): update TanStack packages (#497)

--- rsbuild/tanstack-start-rsc/rsbuild.config.ts
+++ rsbuild/tanstack-start-rsc/rsbuild.config.ts
@@ -4,11 +4,6 @@ import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
 import { tanstackStart } from '@tanstack/react-start/plugin/rsbuild';
 
 export default defineConfig({
-  source: {
-    // RSC needs SWC to compile dependencies so directives like "use client" can be detected.
-    // The TanStack Rsbuild plugin will add this automatically when RSC is enabled in the future.
-    include: [{ not: /[\\/]core-js[\\/]/ }],
-  },
   plugins: [
     pluginReact(),
     pluginTailwindcss(),
